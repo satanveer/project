afb1d9649fe6f25597eb92e072b325c3761209fd
satanveer <satanveersingh@gmail.com>
2022-05-11 22:16:37 +0530

    classworks

---
+++ Classwork/project1.py
@@ -0,0 +1,13 @@
+#aggrigate of 5 subjects 
+import math
+
+
+physics=int(input("Enter marks in phy "))
+chemistry=int(input("Enter marks in chem "))
+maths=int(input("Enter marks in math "))
+english=int(input("Enter marks in eng "))
+IT=int(input("Enter marks in IT "))
+Total=physics+chemistry+maths+english+IT
+print("The total marks are",Total)
+aggrigate=(Total/500)*100
+print("the percentage is ",aggrigate)
